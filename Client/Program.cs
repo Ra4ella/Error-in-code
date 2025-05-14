8b4dd300b5fd7681b1a12d95e92d98522e4202fb
Ra4ella <TTpuBet109@gmail.com>
2025-05-14 21:05:08 +0300

    First push

---
+++ Client/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Client
+{
+    internal class Program
+    {
+        static int port_IT_developer = 7001;
+        static int port_Web_designer = 7002;
+        static int port_Cyber_security = 7003;
+        static string ip_IT_developer = "239.0.0.101";
+        static string ip_Web_designer = "239.0.0.102";
+        static string ip_Cyber_security = "239.0.0.103";
+        static void GetMulticastGroup(string choice)
+        {
+            UdpClient client = new UdpClient();
+            client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+            IPEndPoint clientEP = new IPEndPoint(IPAddress.Any, 0);
+
+            if (choice == "1")
+            {
+                client.Client.Bind(new IPEndPoint(IPAddress.Any, port_IT_developer));
+                client.JoinMulticastGroup(IPAddress.Parse(ip_IT_developer));
+                Console.WriteLine("Підключено до групи: IT Developer");
+            }
+            else if (choice == "2")
+            {
+                client.Client.Bind(new IPEndPoint(IPAddress.Any, port_Web_designer));
+                client.JoinMulticastGroup(IPAddress.Parse(ip_Web_designer));
+                Console.WriteLine("Підключено до групи: Web Designer");
+            }
+            else if (choice == "3")
+            {
+                client.Client.Bind(new IPEndPoint(IPAddress.Any, port_Cyber_security));
+                client.JoinMulticastGroup(IPAddress.Parse(ip_Cyber_security));
+                Console.WriteLine("Підключено до групи: Cyber Security");
+            }
+
+            while (true)
+            {
+                Console.Write("-: ");
+                byte[] data = client.Receive(ref clientEP);
+                string message = Encoding.UTF8.GetString(data);
+                Console.WriteLine($"[Multicast] {message}");
+                Console.Write("-: ");
+            }
+        }
+        static void GetBroadcastMessange()
+        {
+            UdpClient client = new UdpClient();
+            client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            client.Client.Bind(new IPEndPoint(IPAddress.Any, 6001));
+            IPEndPoint clientEP = new IPEndPoint(IPAddress.Any, 0);
+
+            Console.WriteLine("Broadcast підключен...");
+
+            while (true)
+            {
+                byte[] message_Bytes = client.Receive(ref clientEP);
+                string message = Encoding.UTF8.GetString(message_Bytes);
+                Console.WriteLine($"[Broadcast] {message}");
+                Console.Write($"-: ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            TcpClient client = new TcpClient("127.0.0.1", 5000);
+            NetworkStream stream = client.GetStream();
+
+            while (true)
+            {
+                Console.WriteLine($"1. Запитати свій розклад (TCP)");
+                Console.WriteLine($"2. Слухати Broadcast");
+                Console.WriteLine($"3. Приєднатись до Multicast-групи");
+                Console.WriteLine($"4. Вийти");
+                Console.Write($"-: ");
+                string choice = Console.ReadLine();
+                byte[] choice_Bytes = Encoding.UTF8.GetBytes(choice);
+                stream.Write(choice_Bytes, 0, choice_Bytes.Length);
+
+                if (choice == "1")
+                {
+                    byte[] schedule_Bytes = new byte[1024];
+                    int schedule_Int = stream.Read(schedule_Bytes, 0, schedule_Bytes.Length);
+                    string schedule = Encoding.UTF8.GetString(schedule_Bytes);
+                    Console.WriteLine(schedule);
+                }
+                if (choice == "2")
+                {
+                    Thread thread1 = new Thread(() => GetBroadcastMessange());
+                    thread1.Start();
+                }
+                if (choice == "3")
+                {
+                    Console.WriteLine($"1. IT developer");
+                    Console.WriteLine($"2. Web designer");
+                    Console.WriteLine($"3. Cyber security");
+                    Console.Write($"-: ");
+                    string choice2 = Console.ReadLine();
+                    byte[] choice2_Bytes = Encoding.UTF8.GetBytes(choice2);
+                    stream.Write(choice2_Bytes, 0, choice2_Bytes.Length);
+
+                    Thread thread2 = new Thread(() => GetMulticastGroup(choice2));
+                    thread2.Start();
+                }
+                if (choice == "4")
+                {
+                    client.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace Server
+{
+    internal class Program
+    {
+        static int port_IT_developer = 7001;
+        static int port_Web_designer = 7002;
+        static int port_Cyber_security = 7003;
+        static string ip_IT_developer = "239.0.0.101";
+        static string ip_Web_designer = "239.0.0.102";
+        static string ip_Cyber_security = "239.0.0.103";
+        static Random rnd = new Random();
+        static int id = 0;
+        static void TcpSchedule(NetworkStream stream)
+        {
+            string schedule = "Schedule: 1. Math | 2. IT | 3. English";
+            byte[] schedule_Byte = Encoding.UTF8.GetBytes(schedule);
+            stream.Write(schedule_Byte, 0, schedule_Byte.Length);
+        }
+        static void BroadcastMessage()
+        {
+            UdpClient server = new UdpClient();
+            server.EnableBroadcast = true;
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Broadcast, 6001);
+
+            while (true)
+            {
+                Thread.Sleep(1500);
+                string announcement = "In building 1 there isn't water";
+                byte[] announcement_Bytes = Encoding.UTF8.GetBytes(announcement);
+                server.Send(announcement_Bytes, announcement_Bytes.Length, remoteEP);
+                Console.WriteLine($"[Broadcast] Надіслано");
+                Thread.Sleep(1500);
+            }
+        }
+        static void MulticastMessage(string user2_choice)
+        {
+            UdpClient server = new UdpClient();
+            if (user2_choice == "1")
+            {
+                Console.WriteLine("Work");
+                string message = "In group IT developer there isn't third lesson";
+                byte[] message_Bytes = Encoding.UTF8.GetBytes(message);
+                server.Send(message_Bytes, message_Bytes.Length, ip_IT_developer, port_IT_developer);
+                Console.WriteLine("[Multicast] Надіслано в групу IT developer");
+            }
+            else if (user2_choice == "2")
+            {
+                Console.WriteLine("Work");
+                string message = "In group Web designer the class is moved to 4 PM";
+                byte[] message_Bytes = Encoding.UTF8.GetBytes(message);
+                server.Send(message_Bytes, message_Bytes.Length, ip_Web_designer, port_Web_designer);
+                Console.WriteLine("[Multicast] Надіслано в групу Web designer");
+            }
+            else if (user2_choice == "3")
+            {
+                Console.WriteLine("Work");
+                string message = "In group Cyber security there is a teacher replacement";
+                byte[] message_Bytes = Encoding.UTF8.GetBytes(message);
+                server.Send(message_Bytes, message_Bytes.Length, ip_Cyber_security, port_Cyber_security);
+                Console.WriteLine("[Multicast] Надіслано в групу Cyber security");
+            }
+        }
+        static void ServerWork(TcpClient client)
+        {
+            while (true)
+            {
+                NetworkStream stream = client.GetStream();
+
+                byte[] user_choice_Byte = new byte[1024];
+                int user_choice_Int = stream.Read(user_choice_Byte, 0, user_choice_Byte.Length);
+                string user_choice = Encoding.UTF8.GetString(user_choice_Byte, 0, user_choice_Int);
+
+                if (user_choice == "1")
+                {
+                    Thread thread1 = new Thread(() => TcpSchedule(stream));
+                    thread1.Start();
+                    Console.WriteLine("Schedule sent");
+                }
+                if (user_choice == "2")
+                {
+                    Thread thread2 = new Thread(() => BroadcastMessage());
+                    thread2.Start();
+                    Console.WriteLine("Broadcast acctived");
+                }
+                if (user_choice == "3")
+                {
+                    byte[] user_choice2_Byte = new byte[1024];
+                    int user_choice2_Int = stream.Read(user_choice2_Byte, 0, user_choice2_Byte.Length);
+                    string user2_choice = Encoding.UTF8.GetString(user_choice2_Byte, 0, user_choice2_Int);
+                    Thread thread3 = new Thread(() => MulticastMessage(user2_choice));
+                    thread3.Start();
+                    Console.WriteLine("Multicast activated");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            TcpListener server = new TcpListener(IPAddress.Any, 5000);
+            server.Start();
+            Console.WriteLine("Server status: Online");
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine($"Client {++id} online");
+
+                Thread thread = new Thread(() => ServerWork(client));
+                thread.Start();
+            }
+        }
+    }
+}
